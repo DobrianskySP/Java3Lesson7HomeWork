58f9b723e4cf3d8d81b3d4e30537294d043132e4
Станислав <dobriansky.sp@oilpc.ru>
2020-07-06 13:36:19 +0300

    somthing not working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello new branch");
+        try {
+            Method m =Main.class.getMethod("firstTest",null);
+            Annotation[] test = m.getAnnotations(SuperTest.class);
+            System.out.println("Value: " + test[0].value());
+        } catch (NoSuchMethodException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/com/company/MyTestClass.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class MyTestClass {
+    @Test(value = 10)
+    public void firstTest(){
+        System.out.println("Что то работает)");
+    }
+}
